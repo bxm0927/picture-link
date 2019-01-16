d5f72b75cbd11080f255b70ccacb6a2678eba5d8
bxm0927 <80583600@qq.com>
2019-01-17 00:28:39 +0800

    add picture to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # picture-link
 Generate the picture outside chain
+
+https://github.com/bxm0927/picture-link/tree/master/markdown-note
+
+https://github.com/bxm0927/picture-link/blob/master/markdown-note/2019-1-17%20(1).png?raw=true
+
+https://raw.githubusercontent.com/bxm0927/picture-link/master/markdown-note/xxx.ext
--- autoPush.sh
+++ autoPush.sh
@@ -5,4 +5,6 @@
 
 git pull && git add . && git commit -m "add picture to GitHub" && git push
 
-echo "https://github.com/bxm0927/picture-link/tree/master/markdown-note"
+# echo "https://github.com/bxm0927/picture-link/tree/master/markdown-note"
+
+echo "https://raw.githubusercontent.com/bxm0927/picture-link/master/markdown-note/xxx.ext"
